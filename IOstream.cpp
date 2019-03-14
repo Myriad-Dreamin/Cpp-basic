1b025047348b54f595d554503d30ae9722e06e28
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2019-03-14 15:24:19 +0800

    Matrix

--- IOstream.cpp
+++ IOstream.cpp
@@ -7,13 +7,13 @@ using namespace std;
 int main()
 {
     srand(time(nullptr));
-    
+
     int random_price = ((rand() + rand()) % 1000) + 1;
     int input_price = -1;
     while (random_price != input_price)
     {
         cin >> input_price;
-        
+
         if (input_price > random_price)
         {
             cout << "too high!" << endl;
