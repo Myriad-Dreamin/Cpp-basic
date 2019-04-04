fe293f2180d43548771a2ebe06ed11b134446f4b
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2019-04-04 09:09:55 +0800

    splited sort comparsion

--- SortCompare/test/stable/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-%sort% = stable_sort
-../dbg ./big_rand_10_1e4 std_sort %sort% 
-../dbg ./big_rand_1e4_5e5 std_sort %sort%
-../dbg ./big_rand_5e5_1e7 std_sort %sort% 
-../dbg ./big_rand_1e7_1e8 std_sort %sort% 
-../dbg ./big_rand_2e8 std_sort %sort% 
